566ec1282794535a8ec501d6bb56aac8444ca041
Bradley Neve <brad.neve@gmail,com>
2022-10-26 11:35:55 +0100

    add reroute route page

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
-import random
 from flask_sqlalchemy import SQLAlchemy
+import random
+import string
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urlsStore.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
 
 class Urls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,17 +18,38 @@ class Urls(db.Model):
         self.big_url = big_url
         self.small_url = small_url
 
-
 with app.app_context():
     db.create_all()
 
-@app.route("/")
+def get_random_url():
+    letters = string.ascii_lowercase
+    result_url = ''.join(random.choice(letters) for i in range(4))
+    return result_url
+
+
+@app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html", display="none")
+    elif request.method == "POST":
+        url_to_squash = request.form["url_to_squash"]
+        existing_squashed_url = Urls.query.filter_by(big_url=url_to_squash).first()
+        if existing_squashed_url:
+            return render_template("home.html", url=existing_squashed_url.small_url)
+        else:
+            random_url = get_random_url()
+            new_squashed_url = Urls(url_to_squash, random_url)
+            db.session.add(new_squashed_url)
+            db.session.commit()
+            return render_template("home.html", url=f"http://localhost:5000/{random_url}") 
 
-
-
-
+@app.route("/<small_url>")
+def go_to_page(small_url):
+    stored_urls = Urls.query.filter_by(small_url=small_url).first()
+    if stored_urls:
+        return redirect(stored_urls.big_url)
+    else:
+        return f"Url doesn`t exist"
 
 if __name__ == "__main__":
     app.run(debug=True)
